e3e99f9f880b1f391959687e1c55f38db4934944
LEE666bit <madjack7845@naver.com>
2020-11-03 04:14:03 +0900

    upload files

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,290 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다.
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마십시오.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Node0 = new System.Windows.Forms.TextBox();
+            this.Node1 = new System.Windows.Forms.TextBox();
+            this.Node2 = new System.Windows.Forms.TextBox();
+            this.Node3 = new System.Windows.Forms.TextBox();
+            this.Node4 = new System.Windows.Forms.TextBox();
+            this.Node5 = new System.Windows.Forms.TextBox();
+            this.Node6 = new System.Windows.Forms.TextBox();
+            this.Node7 = new System.Windows.Forms.TextBox();
+            this.Node8 = new System.Windows.Forms.TextBox();
+            this.Node9 = new System.Windows.Forms.TextBox();
+            this.Node10 = new System.Windows.Forms.TextBox();
+            this.Node11 = new System.Windows.Forms.TextBox();
+            this.Node12 = new System.Windows.Forms.TextBox();
+            this.Node13 = new System.Windows.Forms.TextBox();
+            this.Node14 = new System.Windows.Forms.TextBox();
+            this.Node15 = new System.Windows.Forms.TextBox();
+            this.playB = new System.Windows.Forms.Button();
+            this.stopB = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.BPM_Box = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Node0
+            // 
+            this.Node0.Location = new System.Drawing.Point(150, 240);
+            this.Node0.Name = "Node0";
+            this.Node0.Size = new System.Drawing.Size(100, 21);
+            this.Node0.TabIndex = 0;
+            this.Node0.Text = "CM7";
+            // 
+            // Node1
+            // 
+            this.Node1.Location = new System.Drawing.Point(350, 240);
+            this.Node1.Name = "Node1";
+            this.Node1.Size = new System.Drawing.Size(100, 21);
+            this.Node1.TabIndex = 1;
+            this.Node1.Text = "C#dim7";
+            // 
+            // Node2
+            // 
+            this.Node2.Location = new System.Drawing.Point(550, 240);
+            this.Node2.Name = "Node2";
+            this.Node2.Size = new System.Drawing.Size(100, 21);
+            this.Node2.TabIndex = 2;
+            this.Node2.Text = "D-7";
+            // 
+            // Node3
+            // 
+            this.Node3.Location = new System.Drawing.Point(750, 240);
+            this.Node3.Name = "Node3";
+            this.Node3.Size = new System.Drawing.Size(100, 21);
+            this.Node3.TabIndex = 3;
+            this.Node3.Text = "G7";
+            // 
+            // Node4
+            // 
+            this.Node4.Location = new System.Drawing.Point(150, 380);
+            this.Node4.Name = "Node4";
+            this.Node4.Size = new System.Drawing.Size(100, 21);
+            this.Node4.TabIndex = 4;
+            this.Node4.Text = "C7";
+            // 
+            // Node5
+            // 
+            this.Node5.Location = new System.Drawing.Point(350, 380);
+            this.Node5.Name = "Node5";
+            this.Node5.Size = new System.Drawing.Size(100, 21);
+            this.Node5.TabIndex = 5;
+            this.Node5.Text = "FM7";
+            // 
+            // Node6
+            // 
+            this.Node6.Location = new System.Drawing.Point(550, 380);
+            this.Node6.Name = "Node6";
+            this.Node6.Size = new System.Drawing.Size(100, 21);
+            this.Node6.TabIndex = 6;
+            this.Node6.Text = "G7";
+            // 
+            // Node7
+            // 
+            this.Node7.Location = new System.Drawing.Point(750, 380);
+            this.Node7.Name = "Node7";
+            this.Node7.Size = new System.Drawing.Size(100, 21);
+            this.Node7.TabIndex = 7;
+            this.Node7.Text = "CM7";
+            // 
+            // Node8
+            // 
+            this.Node8.Location = new System.Drawing.Point(150, 520);
+            this.Node8.Name = "Node8";
+            this.Node8.Size = new System.Drawing.Size(100, 21);
+            this.Node8.TabIndex = 8;
+            this.Node8.Text = "CM7";
+            // 
+            // Node9
+            // 
+            this.Node9.Location = new System.Drawing.Point(350, 520);
+            this.Node9.Name = "Node9";
+            this.Node9.Size = new System.Drawing.Size(100, 21);
+            this.Node9.TabIndex = 9;
+            this.Node9.Text = "FM7";
+            // 
+            // Node10
+            // 
+            this.Node10.Location = new System.Drawing.Point(550, 520);
+            this.Node10.Name = "Node10";
+            this.Node10.Size = new System.Drawing.Size(100, 21);
+            this.Node10.TabIndex = 10;
+            this.Node10.Text = "G7";
+            // 
+            // Node11
+            // 
+            this.Node11.Location = new System.Drawing.Point(750, 520);
+            this.Node11.Name = "Node11";
+            this.Node11.Size = new System.Drawing.Size(100, 21);
+            this.Node11.TabIndex = 11;
+            this.Node11.Text = "A-7";
+            // 
+            // Node12
+            // 
+            this.Node12.Location = new System.Drawing.Point(150, 660);
+            this.Node12.Name = "Node12";
+            this.Node12.Size = new System.Drawing.Size(100, 21);
+            this.Node12.TabIndex = 12;
+            this.Node12.Text = "D7";
+            // 
+            // Node13
+            // 
+            this.Node13.Location = new System.Drawing.Point(350, 660);
+            this.Node13.Name = "Node13";
+            this.Node13.Size = new System.Drawing.Size(100, 21);
+            this.Node13.TabIndex = 13;
+            this.Node13.Text = "Db7";
+            // 
+            // Node14
+            // 
+            this.Node14.Location = new System.Drawing.Point(550, 660);
+            this.Node14.Name = "Node14";
+            this.Node14.Size = new System.Drawing.Size(100, 21);
+            this.Node14.TabIndex = 14;
+            this.Node14.Text = "CM7";
+            // 
+            // Node15
+            // 
+            this.Node15.Location = new System.Drawing.Point(750, 660);
+            this.Node15.Name = "Node15";
+            this.Node15.Size = new System.Drawing.Size(100, 21);
+            this.Node15.TabIndex = 15;
+            this.Node15.Text = "CM7";
+            // 
+            // playB
+            // 
+            this.playB.Font = new System.Drawing.Font("굴림", 40F);
+            this.playB.Location = new System.Drawing.Point(150, 42);
+            this.playB.Name = "playB";
+            this.playB.Size = new System.Drawing.Size(100, 100);
+            this.playB.TabIndex = 16;
+            this.playB.Text = "▶";
+            this.playB.UseVisualStyleBackColor = true;
+            this.playB.Click += new System.EventHandler(this.playB_Click);
+            // 
+            // stopB
+            // 
+            this.stopB.Font = new System.Drawing.Font("굴림", 40F);
+            this.stopB.Location = new System.Drawing.Point(296, 42);
+            this.stopB.Name = "stopB";
+            this.stopB.Size = new System.Drawing.Size(100, 100);
+            this.stopB.TabIndex = 17;
+            this.stopB.Text = "■";
+            this.stopB.UseVisualStyleBackColor = true;
+            this.stopB.Click += new System.EventHandler(this.stopB_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("굴림", 40F);
+            this.label1.Location = new System.Drawing.Point(512, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(138, 54);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "BPM";
+            // 
+            // BPM_Box
+            // 
+            this.BPM_Box.Font = new System.Drawing.Font("굴림", 40F);
+            this.BPM_Box.ImeMode = System.Windows.Forms.ImeMode.Disable;
+            this.BPM_Box.Location = new System.Drawing.Point(656, 39);
+            this.BPM_Box.Name = "BPM_Box";
+            this.BPM_Box.Size = new System.Drawing.Size(100, 69);
+            this.BPM_Box.TabIndex = 19;
+            this.BPM_Box.Text = "200";
+            this.BPM_Box.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.BPM_Box_KeyPress);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 761);
+            this.Controls.Add(this.BPM_Box);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.stopB);
+            this.Controls.Add(this.playB);
+            this.Controls.Add(this.Node15);
+            this.Controls.Add(this.Node14);
+            this.Controls.Add(this.Node13);
+            this.Controls.Add(this.Node12);
+            this.Controls.Add(this.Node11);
+            this.Controls.Add(this.Node10);
+            this.Controls.Add(this.Node9);
+            this.Controls.Add(this.Node8);
+            this.Controls.Add(this.Node7);
+            this.Controls.Add(this.Node6);
+            this.Controls.Add(this.Node5);
+            this.Controls.Add(this.Node4);
+            this.Controls.Add(this.Node3);
+            this.Controls.Add(this.Node2);
+            this.Controls.Add(this.Node1);
+            this.Controls.Add(this.Node0);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Node0;
+        private System.Windows.Forms.TextBox Node1;
+        private System.Windows.Forms.TextBox Node2;
+        private System.Windows.Forms.TextBox Node3;
+        private System.Windows.Forms.TextBox Node4;
+        private System.Windows.Forms.TextBox Node5;
+        private System.Windows.Forms.TextBox Node6;
+        private System.Windows.Forms.TextBox Node7;
+        private System.Windows.Forms.TextBox Node8;
+        private System.Windows.Forms.TextBox Node9;
+        private System.Windows.Forms.TextBox Node10;
+        private System.Windows.Forms.TextBox Node11;
+        private System.Windows.Forms.TextBox Node12;
+        private System.Windows.Forms.TextBox Node13;
+        private System.Windows.Forms.TextBox Node14;
+        private System.Windows.Forms.TextBox Node15;
+        private System.Windows.Forms.Button playB;
+        private System.Windows.Forms.Button stopB;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox BPM_Box;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+using System.Web;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        const int _C = 0;
+        const char C = 'C';
+        const int _D = 2;
+        const char D = 'D';
+        const int _E = 4;
+        const char E = 'E';
+        const int _F = 5;
+        const char F = 'F';
+        const int _G = 7;
+        const char G = 'G';
+        const int _A = 9;
+        const char A = 'A';
+        const int _B = 11;
+        const char B = 'B';
+
+        const char sharp = '#'; const char flat = 'b';
+        const String _7code = "7";
+        const String m7code = "-7";
+        const String M7code = "M7";
+        const String dim7 = "dim7";
+
+        //public Form1 newF;
+        public int _4bit = 0; //4/4박자중 첫번째 박자로 초기화합니다.
+        public int noteR, note3, note5, note7; //근음과 화음입니다.
+        public int nowPlaying; //재생중인 마디의 번호입니다.
+        public String nodeName; //재생중인 마디의 TextBox 이름입니다.
+        SoundPlayer noteSound;
+
+        public Form1(int playingNode)
+        {
+            int noteR = note3 = note5 = note7 = 0;
+            nowPlaying = playingNode;
+            noteSound = null;
+            nodeName = "Node" + nowPlaying.ToString();
+            InitializeComponent();
+        }
+
+        /*
+        public void setNext(Form1 player)
+        {
+            newF = player;
+        }
+
+        public Form1 getNext()
+        {
+            return newF;
+        }*/
+
+        
+        public void IDcode(String txt, int index, int ROOT) // 근음으로부터 화음을 쌓아올립니다.
+        {
+            if (txt.IndexOf(_7code) == index+1)
+            {
+                note3 = (ROOT + 4) % 12;
+                note5 = (ROOT + 7) % 12;
+                note7 = (ROOT + 10) % 12;
+            }
+            if (txt.IndexOf(m7code) == index+1)
+            {
+                note3 = (ROOT + 3) % 12;
+                note5 = (ROOT + 7) % 12;
+                note7 = (ROOT + 10) % 12;
+            }
+            if (txt.IndexOf(M7code) == index+1)
+            {
+                note3 = (ROOT + 4) % 12;
+                note5 = (ROOT + 7) % 12;
+                note7 = (ROOT + 11) % 12;
+            }
+            if (txt.IndexOf(dim7) == index+1)
+            {
+                note3 = (ROOT + 3) % 12;
+                note5 = (ROOT + 6) % 12;
+                note7 = (ROOT + 9) % 12;
+            }
+            return;
+        }
+
+        public int getCode(int ROOT) //textBox에서 코드를 구분합니다.
+        {
+            int i = 0;
+            String code = (this.Controls["Node" + nowPlaying.ToString()] as TextBox).Text;
+            char root = code[i];
+
+            if (root == C)
+            {
+                ROOT = _C;
+            }
+            if (root==D)
+            {
+                ROOT = _D;
+            }
+            if (root==E)
+            {
+                ROOT = _E;
+            }
+            if (root==F)
+            {
+                ROOT = _F;
+            }
+            if (root==G)
+            {
+                ROOT = _G;
+            }
+            if (root==A)
+            {
+                ROOT = _A;
+            }
+            if (root==B)
+            {
+                ROOT = _B;
+            }
+            if (code[i + 1]==sharp)
+            {
+                i++;
+                ROOT++;
+            }
+            if (code[i + 1]==flat)
+            {
+                i++;
+                ROOT--;
+            }
+            IDcode(code, i, ROOT);
+            return ROOT;
+        }
+
+        /*public int[] _sortSound(int[] Snd, int nR, int n3, int n5, int n7)
+        {
+            int baseSound = nR;
+            if(baseSound > n3)
+            {
+                baseSound = n3;
+                Snd[0] = n3; Snd[1] = n5; Snd[2] = n7; Snd[3] = nR;
+            }
+            if (baseSound > n5)
+            {
+                baseSound = n5;
+                Snd[0] = n5; Snd[1] = n7; Snd[2] = nR; Snd[3] = n3;
+
+            }
+            if (baseSound > n7)
+            {
+                baseSound = n7;
+                Snd[0] = n7; Snd[1] = nR; Snd[2] = n3; Snd[3] = n5;
+            }
+            return Snd;
+        }*/
+
+        public SoundPlayer setSound(int note) //음을 지정합니다
+        {
+            SoundPlayer Sound = null;
+            if (note == _C) Sound = new SoundPlayer(@"C:\Piano\0.wav");
+            if (note == 1) Sound = new SoundPlayer(@"C:\Piano\1.wav");
+            if (note == _D) Sound = new SoundPlayer(@"C:\Piano\2.wav");
+            if (note == 3) Sound = new SoundPlayer(@"C:\Piano\3.wav");
+            if (note == _E) Sound = new SoundPlayer(@"C:\Piano\4.wav");
+            if (note == _F) Sound = new SoundPlayer(@"C:\Piano\5.wav");
+            if (note == 6) Sound = new SoundPlayer(@"C:\Piano\6.wav");
+            if (note == _G) Sound = new SoundPlayer(@"C:\Piano\7.wav");
+            if (note == 8) Sound = new SoundPlayer(@"C:\Piano\8.wav");
+            if (note == _A) Sound = new SoundPlayer(@"C:\Piano\9.wav");
+            if (note == 10) Sound = new SoundPlayer(@"C:\Piano\10.wav");
+            if (note == _B) Sound = new SoundPlayer(@"C:\Piano\11.wav");
+            return Sound;
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (nowPlaying > 15) stopB_Click(sender, e);
+
+            noteR = getCode(noteR);
+            int[] sound = { noteR, note3, note5, note7 };
+            Array.Sort(sound); //낮은 음부터 재생하도록 음을 정렬합니다.
+            noteSound = setSound(sound[_4bit]);
+            noteSound.Play();
+            
+            _4bit++;
+            if (_4bit % 4 == 0)
+            {
+                _4bit = 0;
+                nowPlaying++;
+            }
+        }
+
+        private void stopB_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            timer1 = new Timer();
+            timer1.Enabled = false;
+            timer1.Interval = 1000 *60 / Int32.Parse(BPM_Box.Text);
+            timer1.Tick += new EventHandler(timer1_Tick);
+            nowPlaying = 0;
+            _4bit = 0;
+            
+        }
+
+        private void playB_Click(object sender, EventArgs e)
+        {
+            String getBPM = BPM_Box.Text;
+            timer1.Start();
+            timer1.Interval = 1000 *60 / Int32.Parse(BPM_Box.Text);
+
+            
+        }
+
+        private void BPM_Box_KeyPress(object sender, KeyPressEventArgs e) //BPM 텍스트를 숫자만 받도록 합니다.
+        {
+            if(!(char.IsDigit(e.KeyChar) || e.KeyChar == Convert.ToChar(Keys.Back)))    //숫자와 백스페이스를 제외한 나머지를 바로 처리합니다.
+                {
+                    e.Handled = true;
+                }
+        }
+    }
+}
